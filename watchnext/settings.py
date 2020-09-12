64c7e65c44bbb6b7fa6d593b379b77f18826d3c5
rutvikdumre <45359655+rutvikdumre@users.noreply.github.com>
2020-09-12 13:55:36 +0530

    new whitenoise1

--- watchnext/settings.py
+++ watchnext/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
